cb0d775db9a14e718fb328de082c8a715e98502c
quilo-bikcodes <quilo.bikcodes@gmail.com>
2022-11-13 07:19:02 +0530

    Done

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # IRIS-DATASET-ML-MODEL
-
+<meta name="apple-mobile-web-app-title" content="My App">
 ## Run this code snipnet in your terminal before running the code
 This will install all the requirements modules using the **pip package manager**
 
 ```pip install -r requirements.txt``` 
+
+## To start the program
+```python main.py```
+Use the abpve snippet in terminal.
 
 ### This repo is maintaned by Bikcodes
 Developer  <br />
--- functions.py
+++ functions.py
@@ -11,13 +11,13 @@ from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import StandardScaler
 
 class pltgraph:
 
     def __init__(self,data):
         self.data = data
-        self.labels = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
+        self.params = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
 
     def get_stats(self):
         return self.data.describe()
@@ -36,18 +36,18 @@ class pltgraph:
         plt.show()
    
     def get_linepltSLcm(self):
-        self.showlineplt(self.labels[0])
+        self.showlineplt(self.params[0])
     def get_linepltSWcm(self):
-        self.showlineplt(self.labels[1])
+        self.showlineplt(self.params[1])
     def get_linepltPLcm(self):
-        self.showlineplt(self.labels[2])
+        self.showlineplt(self.params[2])
     def get_linepltPWcm(self):
-        self.showlineplt(self.labels[3])
+        self.showlineplt(self.params[3])
 
 
     def get_pairplt(self):
         tmp = self.data.drop('Id', axis=1)
-        g = sns.pairplot(tmp, hue='Species', markers='+')
+        g = sns.pairplot(tmp, hue='Species', markers='+',kind="reg")
         plt.show()
     def get_pairpltreg(self):
         tmp = self.data.drop('Id', axis=1)
@@ -55,13 +55,50 @@ class pltgraph:
         plt.show()
     
     def get_violinpltSLcm(self):
-        self.showViolinPlt(self.labels[0])
+        self.showViolinPlt(self.params[0])
     def get_violinpltSWcm(self):
-        self.showViolinPlt(self.labels[1])
+        self.showViolinPlt(self.params[1])
     def get_violinpltPLcm(self):
-        self.showViolinPlt(self.labels[2])
+        self.showViolinPlt(self.params[2])
     def get_violinpltPWcm(self):
-        self.showViolinPlt(self.labels[3])
+        self.showViolinPlt(self.params[3])
+
+class MlPrediction:
+    def __init__(self,data):
+        self.data = data
+        self.params = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
+    
+    def KNNalgo(self, K):
+        Scaler = StandardScaler()
+        
+        
+        X = self.data.drop(['Id', 'Species'], axis=1) # Indeependent variable
+        Y = self.data['Species'] # Dependent variable
+        Scaled_X = Scaler.fit_transform(X)
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        classifier = KNeighborsClassifier(n_neighbors=K)
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        return metrics.accuracy_score(y_test, y_pred)
+    
+
+    def LogRegalgo(self):
+        X = self.data.drop(['Id', 'Species'], axis=1) # Indeependent variable
+        Y = self.data['Species'] # Dependent variable
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        classifier = LogisticRegression()
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+        print(y_pred)
+        return metrics.accuracy_score(y_test, y_pred)
+
+iris_df = pd.read_csv('./data/iris.csv')
+obj = MlPrediction(iris_df)
+# a = obj.KNNalgo(36)
+b = obj.LogRegalgo()
+# print(str(a*100)+"%")
+print(str(b*100)+"%")
 
     
 
